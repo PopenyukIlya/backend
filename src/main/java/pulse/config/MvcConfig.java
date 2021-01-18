75cf7a5e7c7b633028d0658519f80da3f94d363f
ilya <popenyukilya@gmail.com>
2021-01-19 00:29:13 +0300

    added

--- src/main/java/pulse/config/MvcConfig.java
+++ src/main/java/pulse/config/MvcConfig.java
@@ -1,6 +1,5 @@
 package pulse.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -13,7 +12,7 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("https://pacific-fjord-28473.herokuapp.com")
                 .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH", "OPTIONS");
 
     }
